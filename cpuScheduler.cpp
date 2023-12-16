4e8b79e414fa82a377ffb9f1af984dbc62927c9b
QaniAhmed <almualim0@gmail.com>
2023-12-16 09:35:14 -0500

    design the logo using ASCII text and write the user interface

---
+++ cpuScheduler.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+using namespace std;
+int main()
+{
+	int choise;
+printf("\n");
+
+printf("      CCCCCCCCCCCCCPPPPPPPPPPPPPPPPP   UUUUUUUU     UUUUUUUU           /////// SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS     \n");
+printf("     CCC::::::::::::CP::::::::::::::::P  U::::::U     U::::::U          /:::::/SS:::::::::::::::S SS:::::::::::::::S  \n");
+printf("   CC:::::::::::::::CP::::::PPPPPP:::::P U::::::U     U::::::U         /:::::/S:::::SSSSSS::::::SS:::::SSSSSS::::::S  \n");
+printf("  C:::::CCCCCCCC::::CPP:::::P     P:::::PUU:::::U     U:::::UU        /:::::/ S:::::S     SSSSSSSS:::::S     SSSSSSS  \n");
+printf(" C:::::C       CCCCCC  P::::P     P:::::P U:::::U     U:::::U        /:::::/  S:::::S            S:::::S               \n");
+printf("C:::::C                P::::P     P:::::P U:::::D     D:::::U       /:::::/   S:::::S            S:::::S               \n");
+printf("C:::::C                P::::PPPPPP:::::P  U:::::D     D:::::U      /:::::/     S::::SSSS          S::::SSSS            \n");
+printf("C:::::C                P:::::::::::::PP   U:::::D     D:::::U     /:::::/       SS::::::SSSSS      SS::::::SSSSS       \n");
+printf("C:::::C                P::::PPPPPPPPP     U:::::D     D:::::U    /:::::/          SSS::::::::SS      SSS::::::::SS     \n");
+printf("C:::::C                P::::P             U:::::D     D:::::U   /:::::/              SSSSSS::::S        SSSSSS::::S    \n");
+printf("C:::::C                P::::P             U:::::D     D:::::U  /:::::/                    S:::::S            S:::::S   \n");
+printf(" C:::::C       CCCCCC  P::::P             U::::::U   U::::::U /:::::/                     S:::::S            S:::::S   \n");
+printf("  C:::::CCCCCCCC::::CPP::::::PP           U:::::::UUU:::::::U/:::::/          SSSSSSS     S:::::SSSSSSSS     S:::::S   \n");
+printf("   CC:::::::::::::::CP::::::::P            UU:::::::::::::UU/:::::/           S::::::SSSSSS:::::SS::::::SSSSSS:::::S   \n");
+printf("     CCC::::::::::::CP::::::::P              UU:::::::::UU /:::::/            S:::::::::::::::SS S:::::::::::::::SS    \n");
+printf("        CCCCCCCCCCCCCPPPPPPPPPP                UUUUUUUUU  ///////              SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSS      \n\n\n\n");
+
+                                                                                                                                                                                                                                                                                                        
+printf("1)Scheduling Method(None)\n2)Preemptive Mode(Off)\n3)Show Result\n4)End Program\nOption>");
+
+ 
+
+                                                                                                                                                                                   
+                                                                                                                                                                                        
+                                                                                                                                                                                        
+                                                              
+                                                              
+                                                              
+                                                                                                                                                                                                                           
+                                                                                                                                                                                                          
+                                     
+
+                                                                      
+                                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                
+                                                                                                                                                                                                
+                                                                                                                                                                                                            
+         
+
+                                                                                                                                                                                                             
+                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                    
+                                                          
+                                                          
+                                                          
+
+
+
+}
