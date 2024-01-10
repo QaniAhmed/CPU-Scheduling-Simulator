1349125b1631e750c63eee2ad447525a08737d13
QaniAhmed <almualim0@gmail.com>
2024-01-10 18:52:48 -0500

    #3 : SJF-nonpreemtive : using data structure and finish by first sort depend on Burst time then select process with min arrival time

--- cpuScheduler.cpp
+++ cpuScheduler.cpp
@@ -5,12 +5,12 @@
 #include <string.h>
 
 using namespace std;
-
 struct node {
 	int process;
 	int Burst;
 	int arrival;
 	int wt;
+	bool done=false;
 	
 	struct node * next;
 };
@@ -117,6 +117,85 @@ int findMinarrivalTime(struct node* temp,int maxArrivalAime)
 	}
 	return minarr;
 }
+//sort depending on Burst Time
+struct node * BurstBubbleSort(struct node* header)
+{
+	struct node *lastnode ,*currentNode ,*nextNode;
+	int tempProcess,tempBurst,tempArrival,tempWait;
+	
+	for (lastnode=NULL; lastnode!=header->next; lastnode=currentNode)
+	{
+		for (currentNode=header; currentNode->next!=lastnode ;currentNode=currentNode->next)
+		{
+			nextNode=currentNode->next;
+			if (currentNode->Burst > nextNode->Burst )
+			{
+				tempProcess=currentNode->process;
+				tempBurst=currentNode->Burst;
+				tempArrival=currentNode->arrival;
+				
+				currentNode->process=nextNode->process;
+				currentNode->Burst=nextNode->Burst;
+				currentNode->arrival=nextNode->arrival;
+				
+				nextNode->process=tempProcess;
+				nextNode->Burst=tempBurst;
+				nextNode->arrival=tempArrival;	
+			}
+//			if (currentNode->Burst == nextNode->Burst && currentNode->arrival > nextNode->arrival)
+//			{
+//				tempProcess=currentNode->process;
+//				tempBurst=currentNode->Burst;
+//				tempArrival=currentNode->arrival;
+//				
+//				currentNode->process=nextNode->process;
+//				currentNode->Burst=nextNode->Burst;
+//				currentNode->arrival=nextNode->arrival;
+//				
+//				nextNode->process=tempProcess;
+//				nextNode->Burst=tempBurst;
+//				nextNode->arrival=tempArrival;	
+//			}
+		}
+		
+	}
+	return header;
+
+}
+//sort depend on process to show result in order
+struct node * ProcessBurstBubbleSort(struct node* header)
+{
+	struct node *lastnode ,*currentNode ,*nextNode;
+	int tempProcess,tempBurst,tempArrival,tempWait;
+	
+	for (lastnode=NULL; lastnode!=header->next; lastnode=currentNode)
+	{
+		for (currentNode=header; currentNode->next!=lastnode ;currentNode=currentNode->next)
+		{
+			nextNode=currentNode->next;
+			if (currentNode->process > nextNode->process )
+			{
+				tempProcess=currentNode->process;
+				tempBurst=currentNode->Burst;
+				tempArrival=currentNode->arrival;
+				tempWait=currentNode->wt;
+				
+				currentNode->process=nextNode->process;
+				currentNode->Burst=nextNode->Burst;
+				currentNode->arrival=nextNode->arrival;
+				currentNode->wt = nextNode->wt;
+				
+				nextNode->process=tempProcess;
+				nextNode->Burst=tempBurst;
+				nextNode->arrival=tempArrival;	
+				nextNode->wt =tempWait;
+			}
+		}
+		
+	}
+	return header;
+
+}
 struct node* ReadFile(FILE *f,int *maxarr,int *TotalBurstTime,int *process1)
 {
 	int process0=1;
@@ -183,8 +262,8 @@ void Read(char IOpath[] );
 void logo();
 void logo1();
 int FCFS(char*inputFile);
-void sjf(char*inputFile);
-void Priority();
+int sjfNonpreeptive(char * inputFile);
+
 int main(int argc ,char * argv[])
 {
 	
@@ -230,16 +309,16 @@ printf("\n\n\n");
 system("@cls||clear");
 logo();
 bool end =false;
-char schedulingMethod [100]="none",Preemptivemethod [100]="non-Preemptive";             
+char schedulingMethod [100]="none",Preemptivemethod [100]="Preemptive";             
 while (end==false){                                                                                                                                                                                                                                                      
 printf("\n1)Scheduling Method(%s)\n2)Preemptive Mode(%s)\n3)Show Result\n4)End Program\nOption>",schedulingMethod,Preemptivemethod);
 scanf("%d",&choise);
-int method=0,columns=0;
+int method=0,methodcpu=0,columns=0;
 bool back=false;
 int proc=0,count=1;
 			char c;
 int times [] ={0,1,1,2,3};
-
+int methodSelected;
 switch (choise)
 {
 	case 1:
@@ -247,28 +326,32 @@ switch (choise)
 	while(back!=true){
 		printf("\nplese select one of the methods below :\n");
 		printf("(1) First Come, First Served Scheduling\n(2) Shortest-Job-First Scheduling\n(3)Priority Scheduling\n(4)Round-Robin Scheduling\n(5)Back\nOption>>");
-		scanf("%d",&method);
-		switch (method)
+		scanf("%d",&methodcpu);
+		switch (methodcpu)
 		{
 			case 1:
 				
 				strcpy(schedulingMethod,"\033[0;32mFirst Come, First Served Scheduling\033[0m");
-				printf("\n>> you selcted method: %d,press back <<\n",method);
+				printf("\n>> you selcted method: %d,press back <<\n",methodcpu);
+				methodSelected=methodcpu;
 				break;
 			case 2:
 				
 				strcpy(schedulingMethod,"\033[0;32mShortest-Job-First Scheduling\033[0m");
-				printf("\n>> you selcted method: %d,press back <<\n",method);
+				printf("\n>> you selcted method: %d,press back <<\n",methodcpu);
+				methodSelected=methodcpu;
 				break;
 			case 3:
 				
 				strcpy(schedulingMethod,"\033[0;32mPriority Scheduling\033[0m");
-				printf("\n>> you selcted method: %d,press back <<\n",method);
+				printf("\n>> you selcted method: %d,press back <<\n",methodcpu);
+				methodSelected=methodcpu;
 				break;
 			case 4:
 				
 				strcpy(schedulingMethod,"\033[0;32mRound-Robin Scheduling\033[0m");
-				printf("\n>> you selcted method: %d,press back <<\n",method);
+				printf("\n>> you selcted method: %d,press back <<\n",methodcpu);
+				methodSelected=methodcpu;
 				break;
 				
 			case 5:
@@ -277,14 +360,15 @@ switch (choise)
 			logo1();
 			break;
 		}
-		
+
+			
 			}
 		break;	
 	case 2:
 		while(method!=3){
 		
 		printf("\nplese select one of the methods below :\n");
-		printf("(1) Preemptive(Default)\n(2) Non-Preemptive\n(3)Back\n>>>");
+		printf("(1) Preemptive\n(2) Non-Preemptive(Default)\n(3)Back\n>>>");
 		scanf("%d",&method);
 		
 		if (method==2)
@@ -299,10 +383,14 @@ switch (choise)
 	logo1();
 	break;
 	case 3:
-		if (schedulingMethod=="none")
-			printf("select method before");
- 			else 
+		printf("%d",methodSelected);
+		if (methodSelected==0)
+				printf("\033[0;31m\nSelect a method first !\n\033[0m");
+ 			else if(methodSelected==1)
 				FCFS(inputFile);
+			else if(methodSelected==2)
+			sjfNonpreeptive(inputFile);
+				
 	break;
 
 	case 4:
@@ -414,7 +502,7 @@ printf("\t¦Scheduling Method: Shortest Job First – Non-Preemptive¦\n");
 printf("\t+--------------------------------------------------------+\n");
 printf("\nprocess\twatitngTime\n\n");
 displayResult(header);
-printf("Average Waiting Time:  :%f ms\n",TotalaWaitTime/count0);
+printf("\033[0;32mAverage Waiting Time:  :%f ms\033[0m\n",TotalaWaitTime/count0);
 while(back)
 {
 	printf("\n\n\nEnter 0 to Back to the menue >>>>");
@@ -429,5 +517,82 @@ logo1();
 //////////////////////////////////////////////////////////////////////
 }
 
+int  sjfNonpreeptive(char * inputFile)
+{
+	int maxarr=0,minarr,TotalBurstTime=0,back=1;
+	int process;
+struct node *header=NULL;
+	FILE *f;
+f=fopen(inputFile,"r"); 
+display(header);
+    header=ReadFile(f,&maxarr,&TotalBurstTime,&process);
+    display(header);
+//sort depending on Burst Time by using bubble sort
+header=BurstBubbleSort(header);
+printf("<sorted depend on Burst Time >\n");
+display(header);
+///////////////////////////////////////////////////////////////////// 
+//find the minimum arrival time 
+minarr=findMinarrivalTime(header,maxarr);
+printf("minarrivaltime:%d\t maxarrivalTime:%d\t totalarrtime:%d\n",minarr,maxarr,TotalBurstTime);
+    				/*caculate the waiting time*/
+    				struct node * temp=header;
+int time=minarr,count0=countlinklist(header),done,waitTime=0;float TotalaWaitTime=0;
+for (int timer =minarr; timer <= TotalBurstTime;) 
+	{
+	        for (int u = 1; u <= count0; u++) 
+			{
+				if (temp->arrival <= timer  && temp->done !=true) 
+				     {
+				            	waitTime=time-temp->arrival;
+				            	
+				            	if(waitTime<0)
+				            	{
+										waitTime=0;
+										//time++;
+										//timer++;
+								}
+								printf("\n waitTime = time %d - arrival %d = %d \n",time,temp->arrival,waitTime);
+				            	temp->wt=waitTime;
+				            	temp->done=true;
+				            	printf("process arrival [%d] <=? timer[%d]\n",temp->arrival,timer);
+				            printf("\n<<process[%d] at timer[%d] buratTime[%d] arrivalTime[%d] \twait[%d]\n",temp->process,timer,temp->Burst,temp->arrival,temp->wt);
+				            time +=temp->Burst;
+				            
+				            TotalaWaitTime+=waitTime;
+				            printf("\n\tTime>>%d\t\n",time);
+				                done++;
+							timer +=temp->Burst;
+							temp = header;//start again when find the process
+							printf("\nthe header is >>>%d\n",temp->arrival);
+							continue;
+				    }
+				            temp = temp->next;
+				            
+	        }
+	        temp = header;//start again after each itreation
+	        if (time >= TotalBurstTime)break;
+}
 
+		header= ProcessBurstBubbleSort(header);
+		system("@cls||clear");
+printf("\t+------------------------------------------------------+\n");
+printf("\t¦Scheduling Method: Shortest Job First – Non-Preemptive¦\n");
+printf("\t+--------------------------------------------------------+");
+printf("\nprocess\twatitngTime\n\n");
+		displayResult(header);
+		printf("\n\033[0;32mAverage Waiting Time:  :%f ms\033[0m\n",TotalaWaitTime/count0);
+		
+//////////////////////////////////////////////////////////////////////
+while(back)
+{
+	printf("\n\n\nEnter 0 to Back to the menue >>>>");
+scanf("%d",&back);
+}
+system("@cls||clear");
+logo1();
+
+	return 0;
+	
+}
 
